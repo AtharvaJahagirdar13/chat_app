ac6fa76c134eff9a685f285d389680c850567714
AtharvaJahagirdar13 <jahagirdaratharva0@gmail.com>
2026-03-01 10:42:49 +0530

    completed project

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,31 +1,28 @@
-import React from 'react'
-import { useAuthStore } from '../store/useAuthStore'
-import { Link } from 'react-router-dom';
-import { LogOut, MessageSquare, Settings, User } from 'lucide-react';
-
+import { Link } from "react-router-dom";
+import { useAuthStore } from "../store/useAuthStore";
+import { LogOut, MessageSquare, Settings, User } from "lucide-react";
 
 const Navbar = () => {
-  const { logout, authUser} = useAuthStore();
+  const { logout, authUser } = useAuthStore();
 
-  
-    
   return (
-    <div>
-      <header className='bg-base-100 border b border-base-300 fixed w-full top-0 z-40 backdrop-blur-lg bg-base-100/80 '>
-
-      <div className='container mx-auto px-4 h-16'>
-        <div className='flex items-center justify-between h-full'>
-            <div className="flex items-center gap-8">
+    <header
+      className="bg-base-100 border-b border-base-300 fixed w-full top-0 z-40 
+    backdrop-blur-lg bg-base-100/80"
+    >
+      <div className="container mx-auto px-4 h-16">
+        <div className="flex items-center justify-between h-full">
+          <div className="flex items-center gap-8">
             <Link to="/" className="flex items-center gap-2.5 hover:opacity-80 transition-all">
               <div className="size-9 rounded-lg bg-primary/10 flex items-center justify-center">
                 <MessageSquare className="w-5 h-5 text-primary" />
               </div>
-              <h1 className="text-lg font-bold">Chatty</h1>
+              <h1 className="text-lg font-bold">luminex</h1>
             </Link>
           </div>
 
-          <div className='flex items-center gap-2'>
-             <Link
+          <div className="flex items-center gap-2">
+            <Link
               to={"/settings"}
               className={`
               btn btn-sm gap-2 transition-colors
@@ -35,6 +32,7 @@ const Navbar = () => {
               <Settings className="w-4 h-4" />
               <span className="hidden sm:inline">Settings</span>
             </Link>
+
             {authUser && (
               <>
                 <Link to={"/profile"} className={`btn btn-sm gap-2`}>
@@ -48,22 +46,10 @@ const Navbar = () => {
                 </button>
               </>
             )}
-
-
           </div>
-
         </div>
-
       </div>
-
-      </header>
-
-
-
-
-
-    </div>
-  )
-}
-
-export default Navbar
+    </header>
+  );
+};
+export default Navbar;
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,51 +1,48 @@
-import {create} from "zustand"
-import { axiosInstance } from "../lib/axios"
+import { create } from "zustand";
+import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
+import { io } from "socket.io-client";
 
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5002" : "/";
 
-export const useAuthStore = create((set) => ({
-    authUser:null,
-    isSigningUp:false,
-    isLoggingIn:false,
-    isUpdatingProfile:false,
-    isCheckingAuth:true,
+export const useAuthStore = create((set, get) => ({
+  authUser: null,
+  isSigningUp: false,
+  isLoggingIn: false,
+  isUpdatingProfile: false,
+  isCheckingAuth: true,
+  onlineUsers: [],
+  socket: null,
 
-    checkAuth: async() =>{
-        try {
-            const res = await  axiosInstance.get("/auth/check");
-            set ({authUser:res.data})
-            
-        } catch (error) {
-            set ({authUser:null})
-            console.log("error in  checkAuth",error)
+  checkAuth: async () => {
+    try {
+      const res = await axiosInstance.get("/auth/check");
 
-            
-        }
-        finally {
-            set ({isCheckingAuth:false});
-        }
-    },
+      set({ authUser: res.data });
+      get().connectSocket();
+    } catch (error) {
+      console.log("Error in checkAuth:", error);
+      set({ authUser: null });
+    } finally {
+      set({ isCheckingAuth: false });
+    }
+  },
 
-    signup: async (data) => {
-        set({isSigningUp:true});
-        try {
-            const res = await axiosInstance.post("/auth/signup",data);
-            set({authUser: res.data });
-            toast.success("Account Created successfully");
-            
-            
-        } catch (error) {
-            toast.error(error.response.data.message);
-            
-            
-        }finally{
-                set({isSigningUp:false});
-            }
+  signup: async (data) => {
+    set({ isSigningUp: true });
+    try {
+      const res = await axiosInstance.post("/auth/signup", data);
+      set({ authUser: res.data });
+      toast.success("Account created successfully");
+      get().connectSocket();
+    } catch (error) {
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isSigningUp: false });
+    }
+  },
 
-
-    },
-
-    login: async (data) => {
+  login: async (data) => {
     set({ isLoggingIn: true });
     try {
       const res = await axiosInstance.post("/auth/login", data);
@@ -60,20 +57,18 @@ export const useAuthStore = create((set) => ({
     }
   },
 
-    logout: async (data) => {
-        try {
-            await axiosInstance.post("/auth/logout");
-            set({authUser:null});
-            toast.success("Logged out successfully ");
-            
-        } catch (error) {
-            toast.error(error.response.data.message);
-            
-        }
-        
-    },
+  logout: async () => {
+    try {
+      await axiosInstance.post("/auth/logout");
+      set({ authUser: null });
+      toast.success("Logged out successfully");
+      get().disconnectSocket();
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  },
 
-    updateProfile: async (data) => {
+  updateProfile: async (data) => {
     set({ isUpdatingProfile: true });
     try {
       const res = await axiosInstance.put("/auth/update-profile", data);
@@ -87,4 +82,26 @@ export const useAuthStore = create((set) => ({
     }
   },
 
-}))
+  connectSocket: () => {
+    const { authUser } = get();
+    if (!authUser || get().socket?.connected) return;
+
+    const socket = io(BASE_URL, {
+      query: {
+        userId: authUser._id,
+      },
+       withCredentials: true,   
+        transports: ["websocket"],
+    });
+    socket.connect();
+
+    set({ socket: socket });
+
+    socket.on("getOnlineUsers", (userIds) => {
+      set({ onlineUsers: userIds });
+    });
+  },
+  disconnectSocket: () => {
+    if (get().socket?.connected) get().socket.disconnect();
+  },
+}));
